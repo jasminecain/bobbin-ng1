9ae7c7016218db1aa7202d48c3008a73ed2f89b4
Jasmine Quach <xjasminequach@gmail.com>
2017-09-20 20:59:22 -0500

    Nav bar hides when logged out shows when logged in

--- app/scripts/components/header/header-component.js
+++ app/scripts/components/header/header-component.js
@@ -3,11 +3,16 @@
 bobbin.component('headerComponent', {
 
   templateUrl: 'app/scripts/components/header/header.html',
-  controller: function($scope, authFactory, $state) {
+  controller: function($scope, authFactory, $state, $window) {
 
-    $scope.logOut = (user) => {
+    //moved header component into each page due to scoping issues; nav hides until login
+    $scope.currentUser = $window.localStorage.getItem('currentUser');
+    console.log($scope.currentUser);
+
+    $scope.logOut = () => {
       authFactory.logOut($scope.account)
         .then(() => {
+          $window.localStorage.removeItem('currentUser');
           $state.go('root');
         });
     };
